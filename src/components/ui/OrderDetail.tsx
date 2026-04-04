c40314213154ca4a9d1c4b3ffbe9daa7c7e3830f
Aaron Ma <stu1030113@gmail.com>
2026-04-04 16:54:42 +0800

    Add orderdetail

--- src/components/ui/OrderDetail.tsx
+++ src/components/ui/OrderDetail.tsx
@@ -24,20 +24,6 @@ interface OrderDetailCardProps {
   onComplete: () => void;
 }
 
-// const order: OrderDetail = {
-//   id: "038",
-//   customerName: "伊地知虹夏",
-//   phone: "0988122707",
-//   datetime: "2026/09/20 22:08",
-//   note: "",
-//   total: 515,
-//   items: [
-//     { name: "綠豆湯", size: "小", topping: "粉角", qty: 6, subtotal: 210 },
-//     { name: "薏仁湯", size: "大", topping: "無", qty: 4, subtotal: 220 },
-//     { name: "洛神花(瓶)", size: "大", topping: "無", qty: 1, subtotal: 75 },
-//   ],
-// };
-
 export default function OrderDetailCard({
   order,
   onClose,
@@ -58,7 +44,7 @@ export default function OrderDetailCard({
           <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
             <path
               d="M6.4 19L5 17.6L10.6 12L5 6.4L6.4 5L12 10.6L17.6 5L19 6.4L13.4 12L19 17.6L17.6 19L12 13.4L6.4 19Z"
-              fill="black"
+              fill="red"
             />
           </svg>
         </button>
